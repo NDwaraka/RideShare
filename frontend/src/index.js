a9fe48e158cd0f728f6e51c8d307ae0649dd389d
mahithamunjeti <mahithamunjeti@gmail.com>
2025-04-25 23:10:59 +0530

    first bbbbasic homepage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Temporary pages for user and driver
+const UserPage = () => <h1>User Page</h1>;
+const DriverPage = () => <h1>Driver Page</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="/driver" element={<DriverPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Measure performance
 reportWebVitals();
